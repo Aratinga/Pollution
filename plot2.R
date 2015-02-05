b9a1bf5290df0af0f3f9b5ac01716b5639d6fa9e
Aratinga <aratinga77@gmail.com>
2015-02-05 16:07:46 -0500

    Slight changes

--- plot2.R
+++ plot2.R
@@ -3,12 +3,11 @@
 # make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 library(dplyr)
 baltimore <- filter(NEI, fips=="24510")
-summary <- summarise(group_by(baltimore,year), sum(Emissions))
-names(summary) <- c("year","Emissions")
-
+summary <- summarise(group_by(baltimore,year), Emissions = sum(Emissions))
 png("plot2.png", width=480, height=480, units="px", pointsize = 12, type = "quartz")
 par(mar=c(5,5,7,3))
-plot(summary$year, summary$Emissions,pch=2, ylab = "Total pollution, tons", xlab = "year", main = "Total PM2.5 Emission for Baltimore City,\n 1999, 2002, 2005, 2008 showing decrease over time")
+plot(summary$year, summary$Emissions,pch=20, ylab = "Total Emissions, tons", xlab = "year", 
+     main = "Total PM2.5 Emissions for Baltimore City,\n 1999, 2002, 2005, 2008 Showing General Decrease Over Time")
 
 model <- lm(Emissions ~ year, summary)
 abline(model, lwd=2)
